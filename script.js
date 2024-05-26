71fbe4ec975470847eda6511cd8a1474e57c0724
Daniel Reiman <100288199+ChickenRei@users.noreply.github.com>
2024-05-26 21:11:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,77 @@
-
 const texts = [
   "Cipher Command",
   "Welcome to the Cipher Command Download Page.",
   "Choose an option below to download Cipher Command:",
-  "Version: 1.0.0",
+  "Version: 1.0",
   "Developer: Daniel Reiman",
-  "Copyright (C) 2024 Daniel Reiman. All rights reserved."
 ];
 
 let index = 0;
 let textIndex = 0;
 let currentText = "";
-let currentElement = document.querySelectorAll(".typing-animation");
-
+const currentElements = document.querySelectorAll(".typing-animation");
 const keypressSound = document.getElementById("keypress-sound");
-keypressSound.volume = 0.5
+
+keypressSound.volume = 0.5;
+
 function type() {
   if (index < texts.length) {
-    keypressSound.play()
-    currentText = texts[index];
-    currentElement[index].textContent = currentText.substring(0, textIndex + 1); // Append the complete sentence
     keypressSound.play();
+    currentText = texts[index];
+    currentElements[index].textContent = currentText.substring(0, textIndex + 1);
 
-    currentElement.forEach((el, i) => {
+    currentElements.forEach((el, i) => {
       if (i === index) {
         el.classList.add("show-caret");
       } else {
         el.classList.remove("show-caret");
       }
     });
+
     textIndex++;
     if (textIndex < currentText.length) {
-      setTimeout(type, 100); // Adjust the typing speed as needed
+      setTimeout(type, 80);
     } else {
       index++;
       textIndex = 0;
       if (index === 3) {
-        document.querySelector(".buttons").style.display = "block"; // Show buttons after typing "Choose an option below..."
+        document.querySelector(".buttons").style.display = "block";
       }
       if (index < texts.length) {
-        setTimeout(type, 295); // Delay before typing the next text
+        setTimeout(type, 500);
       } else {
-        keypressSound.pause(); 
-        currentElement[currentElement.length - 1].classList.remove("show-caret");
-
+        keypressSound.pause();
+        currentElements[currentElements.length - 1].classList.remove("show-caret");
       }
     }
   }
 }
 
-function startDownload(fileUr) {
-  // Hide buttons
+function startDownload(fileUrl) {
   document.querySelector(".buttons").style.display = "none";
-  // Show progress bar
+
   const progressBar = document.createElement("div");
   progressBar.className = "progress-container";
   const progress = document.createElement("div");
   progress.className = "progress-bar";
   progressBar.appendChild(progress);
   document.querySelector(".content").appendChild(progressBar);
+  progressBar.style.display = "block";
 
   let percent = 0;
-  const interval = setInterval(function() {
-      percent += 10;
-      progress.style.width = percent + "%";
-      progress.textContent = percent + "%";
-      if (percent >= 100) {
-          clearInterval(interval);
-          // Hide progress bar
-          progressBar.style.display = "none";
-          // Show buttons again
-          document.querySelector(".buttons").style.display = "block";
-
-          window.open(fileUr)
-        }
+  const interval = setInterval(() => {
+    percent += 10;
+    progress.style.width = percent + "%";
+    progress.textContent = percent + "%";
+    if (percent >= 100) {
+      clearInterval(interval);
+      progressBar.style.display = "none";
+      document.querySelector(".buttons").style.display = "block";
+      window.open(fileUrl);
+    }
   }, 500);
 }
-// Trigger typing animation on page load
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", () => {
   type();
 });
